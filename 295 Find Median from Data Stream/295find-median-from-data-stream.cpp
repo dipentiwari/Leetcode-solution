ba7a4c0500b3636dadd32f38ede6a1fe16fda3ea
DIPEN <dipentiwari0018@gmail.com>
2026-02-11 04:36:59 +0000

    Code challenge submission for question: 295 Find Median from Data Stream

---
+++ 295 Find Median from Data Stream/295find-median-from-data-stream.cpp
@@ -0,0 +1,44 @@
+class MedianFinder {
+public:
+
+    priority_queue<int>left_max_heap;
+    priority_queue<int,vector<int>,greater<int>> right_min_heap;;
+    MedianFinder()
+    {
+       
+    }
+    void addNum(int num)
+    {
+        if(left_max_heap.empty()||num<left_max_heap.top())
+        {
+            left_max_heap.push(num);
+        }
+        else
+        {
+            right_min_heap.push(num);
+        }
+        if(abs((int)left_max_heap.size()- (int)right_min_heap.size())>1)
+        {
+            right_min_heap.push(left_max_heap.top());
+            left_max_heap.pop();
+        }
+        else if(left_max_heap.size()< right_min_heap.size())
+        {
+                left_max_heap.push(right_min_heap.top());
+                right_min_heap.pop();
+        }  
+    }
+    double findMedian()
+    {
+        if(left_max_heap.size()== right_min_heap.size())
+        {
+            //even number of element in both heap
+            return (double)(left_max_heap.top()+right_min_heap.top())/2;
+        }
+        else // odd number of elements
+        {
+            return left_max_heap.top();
+
+        }    
+    }
+};
