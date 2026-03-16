42a963ef655d9495996a211090f7ae8a0aa85b4e
DIPEN <dipentiwari0018@gmail.com>
2026-03-16 05:28:23 +0000

    Code challenge submission for question: 1352 Product of the Last K Numbers

---
+++ 1352 Product of the Last K Numbers/1352product-of-the-last-k-numbers.cpp
@@ -0,0 +1,37 @@
+class ProductOfNumbers {
+public:
+vector<int> nums;
+int n;
+    ProductOfNumbers() {
+      nums.clear();
+      n=0;  
+    }
+    void add(int num) {
+        if(num==0){
+            nums={};
+            n=0;
+        }
+        else{
+            if(nums.empty()){
+                nums.push_back(num);
+
+            }
+            else{
+                nums.push_back(nums[n-1]*num);
+            }
+            n++;
+        }
+
+    }
+    
+    int getProduct(int k) {
+        if(k>n){
+            return 0;
+        }
+        else if(k==n){
+            return nums[n-1];
+        }
+     return nums[n-1]/nums[n-k-1]; 
+    }
+
+};
