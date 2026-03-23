c018dc9c305bdbf32d30f3e36da90f547d85cd6b
DIPEN <dipentiwari0018@gmail.com>
2026-03-23 05:02:46 +0000

    Code challenge submission for question: 814 Binary Tree Pruning

---
+++ 814 Binary Tree Pruning/814binary-tree-pruning.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+// O(n2)
+class Solution {
+public:
+    // bool isonepresent(TreeNode* node) {
+    //     if (node == NULL)
+    //         return false;
+    //     if (node->val == 1)
+    //         return true;
+
+    //     return isonepresent(node->left) || isonepresent(node->right);
+    // }
+    // TreeNode* pruneTree(TreeNode* root) {
+    //     if (root == NULL)
+    //         return NULL;
+    //     if (!isonepresent(root->left))
+    //         root->left = NULL;
+    //     if (!isonepresent(root->right))
+    //         root->right = NULL;
+
+    //     pruneTree(root->left);
+    //     pruneTree(root->right);
+
+    //     if (root->left == NULL && root->right == NULL && root->val == 0)
+    //         return NULL;
+
+    //     return root;
+    TreeNode* pruneTree(TreeNode* root) {
+        if (root == NULL)
+            return NULL;
+
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (root->left == NULL && root->right == NULL && root->val == 0)
+            return NULL;
+
+        return root;
+    }
+};
