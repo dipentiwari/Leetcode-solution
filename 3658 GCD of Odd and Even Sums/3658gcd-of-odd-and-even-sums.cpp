e8f808fda33f3a00c6da9f331f88e8b9c2d9edc2
DIPEN <dipentiwari0018@gmail.com>
2026-07-22 07:09:28 +0000

    Code challenge submission for question: 3658 GCD of Odd and Even Sums

---
+++ 3658 GCD of Odd and Even Sums/3658gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+// int helper(int odd,int even){
+//     if(odd==0) return even;
+//     else return helper(even%odd,odd);
+
+// }
+
+    int gcdOfOddEvenSums(int n) {
+        // int odd=1,even=2;
+        // int oddsum=0,evensum=0;
+        // for(int i=1;i<n;i++){
+        //     odd+=2;
+        //     oddsum+=odd;
+
+        //     even+=2;
+        //     evensum+=even;
+            
+        // }
+        // oddsum+=1;
+        // evensum+=2;
+        // int sum=helper(oddsum,evensum);
+        // return sum ;
+        return n; 
+    }
+};
