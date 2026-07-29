768aa25aba3f6f8ad0c16b4f809d8dec919a3dc0
DIPEN <dipentiwari0018@gmail.com>
2026-07-29 19:26:53 +0000

    Code challenge submission for question: 875 Koko Eating Bananas

---
+++ 875 Koko Eating Bananas/875koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool check(int speed,vector<int>& piles, int h){
+        int n=piles.size();
+        long long count=0;
+        for(int i=0;i<n;i++){
+        // if(count>h) return false;
+            if(speed>=piles[i]) count++;
+            else if((piles[i]%speed)==0) count+=(long long)(piles[i]/speed);
+            else count+=(long long)(piles[i]/speed + 1);
+        }
+        if(count> (long long) h) return false;
+        return true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int mx=-1;
+        for(int i=0;i<n;i++){
+            mx=max(mx,piles[i]);
+        }
+        int low=1;
+        int high=mx;
+        int mid=high;
+        int ans=-1;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(check(mid,piles,h)==true){
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
+};
