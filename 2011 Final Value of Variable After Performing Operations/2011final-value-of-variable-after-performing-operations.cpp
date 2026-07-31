af917eea84547eb5b1f8d699c84193e4aac0362d
DIPEN <dipentiwari0018@gmail.com>
2026-07-31 05:26:57 +0000

    Code challenge submission for question: 2011 Final Value of Variable After Performing Operations

---
+++ 2011 Final Value of Variable After Performing Operations/2011final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int n=operations.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(operations[i]=="--X" || operations[i]=="X--") count--;
+            else count++;
+        }
+        return count;
+    }
+};
