3d589334a4fc215f7de6145a733beafb3c2e4874
DIPEN <dipentiwari0018@gmail.com>
2026-08-06 07:19:37 +0000

    Code challenge submission for question: 3345 Smallest Divisible Digit Product I

---
+++ 3345 Smallest Divisible Digit Product I/3345smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        int tem=n;
+        int ans=1;
+        while(tem>=n){
+            int temp=tem;
+            int val=temp%10;
+            temp/=10;
+            if(temp!=0) ans=temp%10;
+            int curr=val*ans;
+            if(curr%t==0 && tem>=t ) return tem;
+            else tem++;
+        }
+        return 1;
+    }
+};
