cdbd4024713682c8461627dcca4e4a0eece24805
DIPEN TIWARI <dipentiwari0018@gmail.com>
2026-09-08 23:57:01 +0530

    Time: 19 ms (0.75%) | Memory: 13.4 MB (41.31%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    ListNode* getNode(ListNode* head,int idx){
+        ListNode* temp=head;
+        for(int i=1;i<=idx;i++){
+            temp=temp->next;
+        }
+        return temp;
+    }
+    ListNode* reverseList(ListNode* head) {
+        int n=0;
+        ListNode* temp=head;
+        while(temp){
+            temp=temp->next;
+            n++;
+        }
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            ListNode* left=getNode(head,i);
+            ListNode* right=getNode(head,j);
+            int t=left->val;
+            left->val=right->val;
+            right->val=t;
+            i++;
+            j--;
+        }
+        temp=head;
+        return temp;
+
+        
+    }
+};
