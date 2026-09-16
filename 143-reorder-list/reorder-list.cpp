80a6b3fd999456419b6939a90ecd68ea7f6c909c
DIPEN TIWARI <dipentiwari0018@gmail.com>
2026-09-16 12:28:35 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (51.69%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -1,10 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head){
-        ListNode* curr=head;
+        ListNode*curr=head;
         ListNode* prev=NULL;
         ListNode* Next=NULL;
-        while(curr){
+        while(curr!=NULL){
             Next=curr->next;
             curr->next=prev;
             prev=curr;
@@ -13,33 +23,48 @@ public:
         return prev;
     }
     void reorderList(ListNode* head) {
-        ListNode* temp=head;
         ListNode* slow=head;
         ListNode* fast=head;
         while(fast->next!=NULL && fast->next->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
         }
+        ListNode* a=head;
         ListNode* b=slow->next;
         slow->next=NULL;
         b=reverseList(b);
-        ListNode* a=head;
         ListNode* c=new ListNode(10);
-        ListNode* tempC=c;
         ListNode* tempA=a;
         ListNode* tempB=b;
-        while(tempA && tempB){
+        ListNode* tempC=c;
+        while(tempA!=NULL && tempB!=NULL){
             tempC->next=tempA;
-            tempC=tempC->next;
             tempA=tempA->next;
+            tempC=tempC->next;
 
             tempC->next=tempB;
-            tempC=tempC->next;
             tempB=tempB->next;
+            tempC=tempC->next;
         }
         if(tempB==NULL) tempC->next=tempA;
-        head=c->next;
 
+        head=c->next;
+        
         
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
