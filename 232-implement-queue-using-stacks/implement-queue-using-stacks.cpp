4e70691d4d3b5654a50797880ce81f0abcf669a7
DIPEN TIWARI <dipentiwari0018@gmail.com>
2026-09-18 23:12:27 +0530

    Time: 2 ms (3.52%) | Memory: 10.1 MB (47.20%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -2,44 +2,42 @@ class MyQueue {
 public:
     stack<int> st;
     stack<int> helper;
-    MyQueue(){
+    MyQueue() {
         
-    }    
+    }
+    
     void push(int x) {
-        st.push(x);
+        if(st.size()==0) st.push(x);
+        else{
+            while(st.size()>0){
+                int n=st.top();
+                st.pop();
+                helper.push(n);
+            }
+            st.push(x);
+            while(helper.size()>0){
+                int n=helper.top();
+                helper.pop();
+                st.push(n);
+            }
+        }
     }
     
     int pop() {
-        while(st.size()>0){
-            helper.push(st.top());
-            st.pop();
-        }
-        int x=helper.top();
-        helper.pop();
-        while(helper.size()>0){
-            st.push(helper.top());
-            helper.pop(); 
-        }
+        int x=st.top();
+        st.pop();
         return x;
     }
     
     int peek() {
-        while(st.size()>0){
-            helper.push(st.top());
-            st.pop();
-        }
-        int x=helper.top();
-        while(helper.size()>0){
-            st.push(helper.top());
-            helper.pop();
-        }
+        int x=st.top();
         return x;
+
     }
     
     bool empty() {
         if(st.size()==0) return true;
         return false;
-
     }
 };
 
