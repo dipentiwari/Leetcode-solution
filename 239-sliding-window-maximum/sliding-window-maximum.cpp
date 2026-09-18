4ead7214402262ae3c44f4937ba69be9a9e3deb9
DIPEN TIWARI <dipentiwari0018@gmail.com>
2026-09-19 01:49:18 +0530

    Time: 36 ms (24.42%) | Memory: 139 MB (91.68%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        int n=arr.size();
-        stack<int> st;
-        int ngi[n];
-        ngi[n-1]=n;
-        st.push(n-1);
-        for(int i=n-2;i>=0;i--){
-            while(st.size()!=0 && arr[st.top()]<=arr[i]){
-                st.pop();
-            }
-            if(st.size()==0) ngi[i]=n;
-            else{
-                ngi[i]=st.top();
-            }
-                st.push(i);
-            
-        }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if(k==1) return nums;
+        int n=nums.size();
         vector<int> ans;
-        int j=0;
-        for(int i=0;i<n-k+1;i++){
-            if(j<i) j=i;
-            int mx=arr[j];
-            while(j < i+k){
-                mx=arr[j];
-                if(ngi[j]>=i+k) break;
-                j=ngi[j];
-            }
-            ans.push_back(mx);
+        deque<int>  dq;
+        for(int i=0;i<n;i++){
+            while(dq.size()>0 && nums[i]>nums[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+            int j=i-k+1;
+            while(j>dq.front()) dq.pop_front();
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
         }
         return ans;
     }
