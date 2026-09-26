90a15aace68171915a8a44d353ae65dbd27364e4
DIPEN TIWARI <dipentiwari0018@gmail.com>
2026-09-26 20:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (97.75%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,42 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int levels(TreeNode* root){
         if(root==NULL) return 0;
         return 1+ max(levels(root->left),levels(root->right));
     }
-    void traverse(TreeNode* root,int curr, int value,vector<int>&v){
+    void lOrder(TreeNode* root,vector<vector<int>> &ans,int level){
         if(root==NULL) return;
-        if(curr==value){
-            v.push_back(root->val);
-            return;
-        }
-        traverse(root->left,curr+1,value,v);
-        traverse(root->right,curr+1,value,v);
-    }
-    void levelTraverse(TreeNode* root,vector<vector<int>> &ans){
-        if(root==NULL) return;
-        int n=levels(root);
-        for(int i=1;i<=n;i++){
-            vector<int> v;
-            traverse(root,1,i,v);
-            ans.push_back(v);
-        }
+        ans[level].push_back(root->val);
+        lOrder(root->left,ans,level+1);
+        lOrder(root->right,ans,level+1);
     }
     vector<vector<int>> levelOrder(TreeNode* root) {
+        int n=levels(root);
         vector<vector<int>> ans;
         if(root==NULL) return ans;
-        levelTraverse(root,ans);
+        for(int i=1;i<=n;i++){
+            vector<int> v;
+            ans.push_back(v);
+        }
+        lOrder(root,ans,0);
         return ans;
     }
 };
